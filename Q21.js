304add2481bde8af3045f3940ca8ad75c7e6c1d0
Rizwan Ansari <160248118+ali-rizwaan@users.noreply.github.com>
2024-02-16 23:10:34 -0800

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,23 @@
+"use strict";
+// Create an array of objects
+let foods = [];
+// Create food objects and push them into the array
+const food1 = {
+    name: "Biryani",
+    price: "250",
+    description: "Chicken Biryani"
+};
+foods.push(food1);
+const food2 = {
+    name: "Korma",
+    price: "550",
+    description: "Chicken Korma"
+};
+foods.push(food2);
+const food3 = {
+    name: "Dessert",
+    price: "200",
+    description: "Fudge Brownie"
+};
+foods.push(food3);
+console.log(foods);
